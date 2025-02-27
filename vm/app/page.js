b71892372a1f5b0c97047fab63bc63923fffe7d5
Seth Nickell <snickell@gmail.com>
2025-02-26 15:22:09 -1000

    critical fix is critical

--- vm/app/page.js
+++ vm/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
 
   const onMsgFromUser = async () => {
     setMsgFromUser('')
-    _renderNextFrame(msgFromUser)
+    sendMessage(msgFromUser)
   }
 
   const handleSvgClick = async (event) => {
