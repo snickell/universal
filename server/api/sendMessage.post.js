520a2f68393c00ed0498f3179b31db7d4639f615
Seth Nickell <snickell@gmail.com>
2025-03-01 05:15:35 -1000

    extract systemPrompt.js

--- server/api/sendMessage.post.js
+++ server/api/sendMessage.post.js
@@ -1,8 +1,7 @@
-import { createAgent, MODE_CHEAP } from '~/lib/agentShared'
+import { createAgent } from '@/lib/agentShared'
 
 const agent = createAgent({
   openRouterAPIKey: process.env.UNIVERSAL_OPENROUTER_API_KEY,
-  mode: MODE_CHEAP
 })
 
 export default defineEventHandler(async (event) => {
