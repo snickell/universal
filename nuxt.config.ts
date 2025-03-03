24c6a2c405f81e5891fd473042fab4d422a3c109
Seth Nickell <snickell@gmail.com>
2025-03-03 01:02:47 -1000

    use conventional variable names instead of hardcoding them

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,15 +20,6 @@ export default defineNuxtConfig({
     // NuxtHub (https://hub.nuxt.com/) options for cloudflare deployment
   },
 
-  runtimeConfig: {
-    oauth: {
-      google: {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }
-    }
-  },
-
   modules: [
     '@nuxthub/core',
     'nuxt-auth-utils'
