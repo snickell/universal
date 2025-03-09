a61208ad8c2a9aaeef7bcf60ea59eb256cf37119
Seth Nickell <snickell@gmail.com>
2025-03-08 18:39:26 -1000

    data-use-cached: let the LLM specify that elements should be loaded from cache, for faster response times

--- components/Universal.vue
+++ components/Universal.vue
@@ -6,11 +6,27 @@ import ScreenContainer from './ScreenContainer.vue'
 import HoldPlease from './HoldPlease.vue'
 import SendMessageBar from './SendMessageBar.vue'
 import AuthPopover from './AuthPopover.vue'
-import { USE_HTML } from '~/lib/constants'
+import { USE_HTML, ENABLE_DATA_USE_CACHED } from '~/lib/constants'
 
 const screenHTML = ref('')
 const loading = ref(false)
 const needAuth = ref(false)
+const lastDocRef = ref(null)
+
+// search doc for any nodes with an id and a data-use-cached attribute, and replace their innerHTML
+// in doc with the innerHTML of the corresponding node with the same id= from lastDoc.value
+function replaceDataUseCachedElements({doc, prevDoc}) {
+  doc.querySelectorAll('[id][data-use-cached]').forEach(node => {
+    const id = node.id
+    const nodeFromPrevDocWithSameID = prevDoc.getElementById(id)
+    if (nodeFromPrevDocWithSameID) {
+      console.log(`data-use-cached(node=${node}), replacing with ${lastNode.id}`)
+      node.innerHTML = nodeFromPrevDocWithSameID.innerHTML
+    } else {
+      console.error(`data-use-cached(node=${node}) ERROR, no matching node in lastDoc`)
+    }
+  })
+}
 
 async function sendMessage(msg) {
   loading.value = true
@@ -21,6 +37,12 @@ async function sendMessage(msg) {
     if (USE_HTML) {
       const parser = new DOMParser()
       const doc = parser.parseFromString(newContent, 'text/html')
+      if (ENABLE_DATA_USE_CACHED && lastDocRef.value) {
+        replaceDataUseCachedElements({doc, prevDoc: lastDocRef.value})
+      } else {
+        console.warn("data-use-cached is disabled")
+      }
+      lastDocRef.value = doc
       screenHTML.value = doc.body.innerHTML
     } else {
       screenHTML.value = newContent
