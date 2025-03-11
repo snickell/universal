cc11590ec294c17f4e739077d679f647b7ef8d6b
Seth Nickell <snickell@gmail.com>
2025-03-11 02:10:01 -1000

    link to ticket about nuxt-auth-utils startup error when websocket=true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,15 +8,8 @@ export default defineNuxtConfig({
     devServer: {
       watch: ['lib'],
     },
-    // Enabling websocket: true causes errors (in nuxt-auth-utils maybe?) like:
-    /*
-      ERROR  / Application error: Cannot read properties of undefined (reading 'runtimeConfig')
-
-          at useRuntimeConfig (node_modules/nitropack/dist/runtime/internal/config.mjs:17:27)
-          at _useSession (node_modules/nuxt-auth-utils/dist/runtime/server/utils/session.js:49:27)
-          at getUserSession (node_modules/nuxt-auth-utils/dist/runtime/server/utils/session.js:7:25)
-          at <anonymous> (node_modules/nuxt-auth-utils/dist/runtime/server/plugins/ws.js:5:11)
-    */
+    // Enabling websocket: true causes 2x startup errors in nuxt-auth-utils
+    // see: https://github.com/atinux/nuxt-auth-utils/issues/372
     // experimental: {
     //   websocket: true
     // },
