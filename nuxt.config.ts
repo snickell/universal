c08a0df654d2b77b05c2567d6cc95c23731d0067
Seth Nickell <snickell@gmail.com>
2025-03-10 19:08:18 -1000

    disable websocket: true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,18 @@ export default defineNuxtConfig({
     devServer: {
       watch: ['lib'],
     },
-    experimental: {
-      websocket: true
-    },
+    // Enabling websocket: true causes errors (in nuxt-auth-utils maybe?) like:
+    /*
+      ERROR  / Application error: Cannot read properties of undefined (reading 'runtimeConfig')
+
+          at useRuntimeConfig (node_modules/nitropack/dist/runtime/internal/config.mjs:17:27)
+          at _useSession (node_modules/nuxt-auth-utils/dist/runtime/server/utils/session.js:49:27)
+          at getUserSession (node_modules/nuxt-auth-utils/dist/runtime/server/utils/session.js:7:25)
+          at <anonymous> (node_modules/nuxt-auth-utils/dist/runtime/server/plugins/ws.js:5:11)
+    */
+    // experimental: {
+    //   websocket: true
+    // },
     preset: "cloudflare_durable"
   },
 
