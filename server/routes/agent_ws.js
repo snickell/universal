6b5349bd19f1c6bc55ed5f0b88146d6288f55bca
Seth Nickell <snickell@gmail.com>
2025-03-12 06:53:16 -1000

    use cloudflare:durable:init to fetch UNIVERSAL_OPENROUTER_API_KEY

--- server/routes/agent_ws.js
+++ server/routes/agent_ws.js
@@ -1,19 +1,40 @@
 // Implements a WebSocket that forwards messages between agent.js and agentShared.js
-
 import { createAgent } from '@/lib/agentShared'
+
+// This is annoying, but in prod we'll be running as part of a cloudflare durable object
+// to access UNIVERSAL_OPENROUTER_API_KEY, we need to use the env variable passed to us
+// by cloudflare when it initializes our websocket, NOT process.env.
+//
+// See: https://github.com/nitrojs/nitro/blob/70beec683c935b63b7e802243d4eb696967d9ed1/src/presets/cloudflare/runtime/cloudflare-durable.ts#L41-L44
+import { useNitroApp } from 'nitropack/runtime'
+let agent = null
+if (process.env.UNIVERSAL_OPENROUTER_API_KEY) {
+  // This will work in dev, or in a non-cloudflare prod
+  console.log("Using process.env.UNIVERSAL_OPENROUTER_API_KEY")
+  agent = createAgent({
+    openRouterAPIKey: process.env.UNIVERSAL_OPENROUTER_API_KEY,
+  })
+} else {
+  console.log("Couldn't see process.env.UNIVERSAL_OPENROUTER_API_KEY, trying to fetch via nitro hook cloudflare:durable:init")
+  useNitroApp().hooks.hook('cloudflare:durable:init', (durableObject, {env}) => {
+    console.log('Durable Object initialized:', durableObject)
+    console.log('Env keys:', Object.keys(env))
+    console.log("Using env.UNIVERSAL_OPENROUTER_API_KEY from cloudflare:durable:init")
+    agent = createAgent({
+      openRouterAPIKey: env.UNIVERSAL_OPENROUTER_API_KEY,
+    })
+    console.log('Agent initialized:', agent)
+  })
+}
+
 
 function shortFrameID(frame) {
   return frame.frameID.slice(0, 8)
 }
 
 function truncate(str, len=40) {
-
   return String(str).replace(/\s+/g, ' ').slice(0, len)
 }
-
-const agent = createAgent({
-  openRouterAPIKey: process.env.UNIVERSAL_OPENROUTER_API_KEY,
-})
 
 export default defineWebSocketHandler({
   open(peer) {
