4e1666a9412384aa626e6ced2fb9bc452397523b
Seth Nickell <snickell@gmail.com>
2025-03-12 17:20:40 -1000

    switch event message format to json (ape-ing JS events), use a queue

--- components/ScreenContainer.vue
+++ components/ScreenContainer.vue
@@ -16,23 +16,52 @@ const {sendMessage} = defineProps({
 
 const screenContainer = ref(null)
 
-function sendMessageAboutEvent(event) {
+const isATextInputEl = el =>
+  (el.tagName === 'TEXTAREA')
+  || (el.tagName === 'INPUT' && ['text', 'password', 'email', 'search', 'url', 'tel'].includes(el.type))
+  || el.contentIsEditable
+
+let eventQueue = []
+function queueEventToSendAsMessage({event, sendImmediately = false}) {
   let el = event.target
 
   // The event.target el might not have a id attr if the LLM didn't follow instructions
   // if it doesn't have an id, we walk up the tree until we find a parent el that does have one
   while (el && !el.id) el = el.parentElement
   if (el && el.id) {
-    sendMessage(`${event.type} on element with id="${el.id}"`, {
-      target: el,
+    // don't trigger click events on text input elements, eventually with a high frame
+    // rate we'd want this, to do auto-complete etc, but 95% of the time, you don't want
+    // to wait for a screen redraw when you click in one, you want to enter text and submit 
+    // it later
+    if (event.type == 'click' && isATextInputEl(el)) return
+
+    const universalEvent = {
+      target: { id: el.id },
       type: event.type,
+      at: new Date().toISOString(),
+    }
+
+    if (event instanceof MouseEvent) {
+      universalEvent.button = event.button
+
       // These are relative to el, the first ancestor with an id:
-      offsetX: event.clientX - el.getBoundingClientRect().left,
-      offsetY: event.clientY - el.getBoundingClientRect().top,
-    })
+      universalEvent.offsetX = event.clientX - el.getBoundingClientRect().left
+      universalEvent.offsetY = event.clientY - el.getBoundingClientRect().top
+    }
+
+    eventQueue.push(universalEvent)
+
+    if (sendImmediately) sendQueuedEventsAsMessage()
   } else {
     console.error('ScreenContainer: no element with an id found for event', event)
   }
+}
+
+function sendQueuedEventsAsMessage() {
+  console.log(`sendQueuedEventsAsMessage`, eventQueue)
+  const events = eventQueue
+  eventQueue = []
+  sendMessage(JSON.stringify({events}))
 }
 
 // javascript fires a click before a double-click, it doesn't wait to see if a
@@ -45,15 +74,15 @@ function onClickOrDblClick(event) {
     pendingSingleClickTimeout = setTimeout(() => {
       // pendingSingleClickTimeout expired and no new clicks were received
       // therefore: its just a single click, send the event:
-      sendMessageAboutEvent(event)
       pendingSingleClickTimeout = null
+      queueEventToSendAsMessage({event, sendImmediately: true})
     }, DOUBLE_CLICK_TIMEOUT_MS)
   } else {
     // a dblclick happened before pendingSingleClickTimeout expired
     // therefore: this is a double-click, send the event:
     clearTimeout(pendingSingleClickTimeout)
     pendingSingleClickTimeout = null
-    sendMessageAboutEvent(event)
+    queueEventToSendAsMessage({event, sendImmediately: true})
   }
 }
 
