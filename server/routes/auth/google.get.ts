40ab0c256553e12bd1df2e7f6c7be74763d8f8f3
Seth Nickell <snickell@gmail.com>
2025-03-14 16:43:54 -1000

    google.get.ts: use sub as google_auth_id

--- server/routes/auth/google.get.ts
+++ server/routes/auth/google.get.ts
@@ -4,6 +4,9 @@ export default defineOAuthGoogleEventHandler({
       user: {
         name: user.name,
         email: user.email,
+        // google says to use the "sub" field (a huge string of numbers) as the unique identifier
+        // see: https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
+        google_auth_id: user.sub,
         google: { user, tokens }
       },
       loggedInAt: new Date().toISOString(),
