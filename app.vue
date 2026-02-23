54707a15dfa42a10c64a6c1560b7f8bcba9a81f6
Seth Nickell <snickell@gmail.com>
2026-02-22 20:35:36 -1000

    switch to chaos anarchy A

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
   <Html>
     <Head>
-      <Title>The Universal Program</Title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <Title>CHAOS</Title>
+      <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg?v=2">
+      <link rel="shortcut icon" href="/favicon.ico?v=2">
       <link rel="manifest" href="/site.webmanifest">
     </Head>
     <Body>
